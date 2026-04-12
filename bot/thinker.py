8c13fe810d80ce26cfd8525ffa233ea664269485
mikael <oscar.lehti@outlook.com>
2026-04-12 15:42:07 +0200

    funkar!!!!

--- bot/thinker.py
+++ bot/thinker.py
@@ -7,8 +7,6 @@ def greenLetters(greenInput):
             greenFakeDict.append((letter,[v for v, x in enumerate(greenInput) if x == letter]))
     return greenFakeDict
 
-# problem med ord som "dread" pga index systemet
-# jämför längd mellan input index lista och ord index lista? å om ordets e längre granska då att de i input versionen stämmer?
 def greenFilter(greenDict):
     result = []
     with open("prep/backup/wordlelist.txt", "r") as reference:
@@ -38,11 +36,8 @@ def greenFilter(greenDict):
                     break
             if auth == True:
                 result.append(reformat)
-    print(result[0])
     return result
 
-
-# måste fixas senare
 def greenYellow(yellow, green):
     refList = []
     for letter in yellow:
@@ -53,16 +48,12 @@ def greenYellow(yellow, green):
                 pass
     return refList
 
-# samma här
 def yellowFilter(yellowInput, greenDict, firstRound):
     yellowList = [letter for letter in yellowInput if letter != "," and letter != " "]
     result = []
     for word in firstRound:
         lettercheck = greenYellow(yellowList, greenDict)
-        print(word)
-        print(lettercheck)
         remaining = [char for char in word]
-        print(remaining)
         auth = True
         offset = 0
         for letter in yellowList:
@@ -70,17 +61,16 @@ def yellowFilter(yellowInput, greenDict, firstRound):
                 for char in greenDict:
                     if lettercheck == []:
                         break
-                    for index in char[1]:
-                        if remaining[index - offset] == char[0]:
-                            remaining.pop(index)
-                            offset += 1
-                            lettercheck.remove(char[0])
-                            print(remaining)
-                            print(lettercheck)
-                            if lettercheck == []:
-                                break
-                        else:
-                            pass
+                    else:
+                        for index in char[1]:
+                            if remaining[index - offset] == char[0]:
+                                remaining.pop(index)
+                                offset += 1
+                                lettercheck.remove(char[0])
+                                if lettercheck == []:
+                                    break
+                            else:
+                                pass
             if letter not in remaining:
                 auth = False
                 break
@@ -88,28 +78,35 @@ def yellowFilter(yellowInput, greenDict, firstRound):
                 remaining.remove(letter)
         if auth == True:
             result.append(word)
-    print(result)
     return result
 
-
-    
+def printer(result):
+    if len(result) < 21:
+        count = 0
+        while count < 19:
+            try:
+                print(result[count])
+                count += 1
+            except IndexError:
+                if result == []:
+                    print("Inga passande ord hittades!")
+                count += 100
+    else:
+        print("Det finns för många alternativ kvar!")
 
 def thinker():
     print("Välkommen till wordle-tänkaren!")
     print("Skriv bokstäverna där de är gröna och sätt '_' på ställen där de inte är")
     greenInput = input("Skriv här: ").lower()
     greenDict = greenLetters(greenInput)
-    print(greenDict)
     firstRound = greenFilter(greenDict)
-    print("Skriv gula bokstäver här i formatet: x, y, z")
+    print("Skriv gula bokstäver här i någon av formaten: x, y, z / xyz")
     print("Om bokstaven förekommer mer än en gång samtidigt som gul skriv den 2 gånger")
     yellowInput = input("Skriv här: ").lower()
     secondRound = yellowFilter(yellowInput, greenDict, firstRound)
-
-    
+    printer(secondRound)
 
 def main():
     thinker()
 
 main()
-
